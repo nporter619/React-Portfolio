74de2be8bbac2ffe52a5a135e4b072d24d038c7e
nporter <nporterwork@gmail.com>
2023-08-28 21:18:42 -0700

    more changes to file structure with basic app.jsx added

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer';
+import AboutMe from './components/AboutMe/AboutMe';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contact from './components/Contact/Contact';
+import Resume from './components/Resume/Resume';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={AboutMe} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ React Portfolio/src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutMe.css';
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <p>This is the About Me section.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
